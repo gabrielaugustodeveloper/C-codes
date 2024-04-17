e8b5bec9f62374baae91c67bf6cec5a48b5b6904
gabrielaugustodeveloper <augusto.dev.g@gmail.com>
2024-04-16 21:55:08 -0300

    Update ex001.c

--- SchoolUN1/LIST0102/ex001.c
+++ SchoolUN1/LIST0102/ex001.c
@@ -14,7 +14,7 @@ printf("Soma: %d\n", num1 + num2);
 printf("Subtracao: %d\n", num1 - num2);
 printf("Multiplicacao: %d\n", num1 * num2);
 printf("Divisao: %d\n", num1 / num2);
-printf("Resto da divisao: %d\n", num1 % num2);
+printf("Resto da Divisao: %d\n", num1 % num2);
 
 
 }
